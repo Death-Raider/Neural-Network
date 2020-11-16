06b311e94796a405af4fc35252922130cadb380e
Death-Raider <65229622+Death-Raider@users.noreply.github.com>
2020-11-16 22:17:43 +0530

    Update Convolution.js

--- convolution/Convolution.js
+++ convolution/Convolution.js
@@ -16,8 +16,8 @@ function Convolution({matrix,filter,bias = 0,step = {x:1,y:1},padding = 0,type="
       for(let j = pad; j < matrix[i-pad].length + pad; j++) padded_matrix[i][j] = matrix[i-pad][j-pad];
     return padded_matrix;
   }
-  //special case for max-pool
-  if(type==="max-pool"){
+  //special case for max_pool
+  if(type==="max_pool"){
     filter = [[0,0],[0,0]];
     bias = 0;
     step = {x:2,y:2}
@@ -25,7 +25,7 @@ function Convolution({matrix,filter,bias = 0,step = {x:1,y:1},padding = 0,type="
   if(matrix === undefined || filter === undefined) throw "Err: Input matrix or filter not specified"
   //adds padding to matrix
   matrix = padMatrix(matrix,padding,0)
-  let mask = []; //use to create a mask when max-pooling for gradient transfer
+  let mask = []; //use to create a mask when max_pooling for gradient transfer
   let output = []; //convoluted output
   let outputSize = {
     y : 1+(matrix.length - filter.length)/step.y,
@@ -34,25 +34,13 @@ function Convolution({matrix,filter,bias = 0,step = {x:1,y:1},padding = 0,type="
   //checking if convolution is possible
   if(outputSize.y-Math.floor(outputSize.y) !== 0 || outputSize.x-Math.floor(outputSize.x) !== 0 ) throw "Err: size not compatible";
   //types of convolutions
-  if(type==="full"){
-    //sliding one matrix over another from right to left, bottom to top
-    for(let y = outputSize.y-1; y > -1 ; y--){
-      output[y] = [];
-      for(let x = outputSize.x-1; x > -1 ; x--) output[y][x] = Activation[0](filterDot(x,y,matrix,filter) + bias);
-    }
-  }else if(type==="conv"){
-    //sliding one matrix over another from left to right, top to bottom
-    for(let y = 0; y < outputSize.y; y++){
-      output[y] = [];
-      for(let x = 0; x < outputSize.x; x++) output[y][x] = Activation[0](filterDot(x,y,matrix,filter) + bias);
-    }
-  }
-  else if(type==="max-pool"){
-    //max pooling and creating mask
-    for(let y = 0; y < outputSize.y; y++){
-      output[y] = [];
-      mask[y] = []
-      for(let x = 0; x < outputSize.x; x++){
+  for(let y = 0; y < outputSize.y; y++){
+    output[y] = [];
+    mask[y] = []
+    for(let x = 0; x < outputSize.x; x++){
+      if(type==="conv") output[y][x] = Activation[0](filterDot(x,y,matrix,filter) + bias);
+      else if(type==="full_conv")output[outputSize.y-(y+1)][outputSize.x-(x+1)] = Activation[0](filterDot(outputSize.x-(x+1),outputSize.y-(y+1),matrix,filter) + bias);
+      else if(type==="max_pool"){
         mask[y][x] = {value:0,position:{x:0,y:0}};
         for(let f1 = 0; f1 < 2; f1++){
           for(let f2 = 0; f2 < 2; f2++){
@@ -65,10 +53,10 @@ function Convolution({matrix,filter,bias = 0,step = {x:1,y:1},padding = 0,type="
         }
         output[y][x] = mask[y][x].value + bias
       }
+      else throw "Err: unidentified type"
     }
   }
-  else throw "Err: unidentified type"
-  return (type==="max-pool")?[output,mask.flat()]:output
+  return (type==="max_pool")?[output,mask.flat()]:output
 }
 //func for backprop, use coming later
 function Rotate(matrix){ // rotates matrix by 180
@@ -81,10 +69,11 @@ function Rotate(matrix){ // rotates matrix by 180
   return matrix
 }
 
+
 let kernal = [
-  [0,1,0],
-  [1,-5,1],
-  [0,1,0]
+  [-1*2,-1*2,-1*2],
+  [-1*2,8*2,-1*2],
+  [-1*2,-1*2,-1*2]
 ];
 let stride = {x:1,y:1};
 //input image (pic of 2)
@@ -114,14 +103,14 @@ let convo1 = Convolution({
 })
 let convo2 = Convolution({
   matrix : convo1,
-  filter : kernal,
-  step : stride,
-  padding : 0,
-  type : "conv"
+  // filter : kernal,
+  // step : stride,
+  // padding : 0,
+  type : "max_pool"
 })
 save("image",image);
 save("convo1",convo1);
-save("convo2",convo2);
+save("convo2",convo2[0]);
 
 function save(y,x){
   const fs = require('fs')

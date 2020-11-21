181b0b90cdc9bf4a1e05e6d5b11fcfe63a73c8bd
Death-Raider <65229622+Death-Raider@users.noreply.github.com>
2020-11-21 18:55:36 +0530

    Update Neural-Network-test.js

--- convolution/Neural-Network-test.js
+++ convolution/Neural-Network-test.js
@@ -1,14 +1,14 @@
 class NeuralNetwork {
-  constructor({input_nodes,layer_count,output_nodes,weight_bias_initilization_range=[-0.001,0.001]} = {}){
+  constructor({input_nodes,layer_count,output_nodes,weight_bias_initilization_range=[-0.001,0.001] } = {}){
     if(input_nodes === undefined || layer_count === undefined || output_nodes === undefined) throw "Error: structural values not given"
     let parameters=createParameters(input_nodes,layer_count,output_nodes,weight_bias_initilization_range[0],weight_bias_initilization_range[1]);
     const copyRadar3D = (y,z = []) =>{for (let _a in y) for (let _b in y[_a]) {if(!z[_a]) z[_a] = []; z[_a][_b] = y[_a][_b].slice();};return z}
     const copyRadar2D = (y,z = []) =>{for (let _a in y){if(!z[_a]) z[_a] = []; z[_a] = y[_a].slice();};return z}
     this.HiddenLayerCount=layer_count;
     this.Weights = copyRadar3D(parameters[0])
-    this.WeightUpdates = copyRadar3D(parameters[0]) // weight gradients
+    this.WeightUpdates = copyRadar3D(parameters[0])
     this.Bias = copyRadar2D(parameters[1])
-    this.BiasUpdates = copyRadar2D(parameters[1]) // bias gradients
+    this.BiasUpdates = copyRadar2D(parameters[1])
     this.Activation = {
       hidden:[(x)=>(x>0)?x:x*0.1,(x)=>(x>0)?1:0.1],
       output:[(x)=>1/(1+Math.exp(-x)),(x)=>x*(1-x)]
@@ -41,7 +41,7 @@ class NeuralNetwork {
   changes(Desired,Output,DerivativeActivation){//backword pass
     let cost=0;
     for(let i = 0; i < Desired.length; i++) cost+=0.5*Math.pow(Output[i]-Desired[i],2);
-    for(let i = 0; i < this.Nodes[this.HiddenLayerCount.length + 1].length; i++){//iterates for last layer
+    for(let i = 0; i < this.Nodes[this.HiddenLayerCount.length + 1].length; i++){
       this.BiasUpdates[this.Weights.length-1][i]=(this.Nodes[this.HiddenLayerCount.length+1][i]-Desired[i])*DerivativeActivation[1](this.Nodes[this.HiddenLayerCount.length+1][i]);
       for(let j = 0; j < this.Nodes[this.HiddenLayerCount.length].length; j++) this.WeightUpdates[this.Weights.length-1][i][j]=(this.BiasUpdates[this.Weights.length-1][i]*this.Nodes[this.HiddenLayerCount.length][j]);
     }
@@ -69,7 +69,7 @@ class NeuralNetwork {
       }
     }
   }
-  trainBatch({TotalTrain=0,trainFunc=()=>{},TotalVal=0,validationFunc=()=>{},learning_rate=0.0005,batch_train = 1,batch_val = 1} = {}){
+  train({TotalTrain=0,trainFunc=()=>{},TotalVal=0,validationFunc=()=>{},learning_rate=0.0005,batch_train = 1,batch_val = 1} = {}){
     let cost=[], cost_val=[], changing = [];
     for(let i = 0; i < parseInt((TotalTrain/batch_train+TotalVal/batch_val)); i++){
       let batch = (i < parseInt(TotalTrain/batch_train))?batch_train:batch_val;
@@ -151,9 +151,272 @@ class NeuralNetwork {
     this.Weights = model[1];
   }
 }
+class ConvolutionNeuralNetwork {
+  constructor({conv_layers = [],feature_maps = [],strides = []}={}){
+    this.network = conv_layers;
+    this.strides = strides;
+    feature_maps.map(e=>{
+      if(e!==1)throw "Err: Currently only 1 feature map supported per layer"
+    })
+    this.featureMapCount = feature_maps;
+    this.Filters = [];
+    this.ConvLayers;
+    this.learning_rate;
+    this.FullyConnected;
+    this.FCshape;
+    this.LayerGrad;
+    this.FilterGrad;
+    for(let i = 0; i < this.network.length; i++){
+      let layer = this.network[i];
+      this.Filters[i] = this.createMatrix(layer[layer.length-1],layer[layer.length-1],this.featureMapCount[i]);
+    }
+  }
+  Convolution({matrix,filter,bias = 0,step = {x:1,y:1},padding = 0,type="conv",activation = "linear"} = {}){
+    let activationBank = {
+      relu:[(x)=>(x>0)?x:x*0.1,(x)=>(x>0)?1:0.1],
+      sigmoid:[(x)=>1/(1+Math.exp(-x)),(x)=>x*(1-x)],
+      linear:[(x)=>x,(x)=>1]
+    };
+    let Activation = activationBank[activation.toLowerCase()] //linear activation and derivative
+    //dot product of filter and the selected image section
+    const filterDot = (x,y,M,F) => {
+      let sum = 0;
+      for(let f1 = 0; f1 < F.length; f1++){
+        for(let f2 = 0; f2 < F[f1].length; f2++){
+          sum += F[f1][f2]*M[f1+y*step.y][f2+x*step.x];
+        }
+      }
+      return sum;
+    }
+    // apply padding to matrix
+    const padMatrix = (matrix,pad,val) => {
+      let padded_matrix = new Array(matrix.length + 2*pad).fill(0).map(e=>new Array(matrix[0].length + 2*pad).fill(val));
+      for(let i = pad; i < matrix.length + pad; i++)
+        for(let j = pad; j < matrix[i-pad].length + pad; j++) padded_matrix[i][j] = matrix[i-pad][j-pad];
+      return padded_matrix;
+    }
+    //special case for max_pool
+    if(type==="max_pool"){
+      filter = [[0,0],[0,0]];
+      bias = 0;
+      step = {x:2,y:2}
+    }
+    if(matrix === undefined || filter === undefined) throw "Err: Input matrix or filter not specified"
+    //adds padding to matrix
+    matrix = padMatrix(matrix,padding,0)
+    let mask = []; //use to create a mask when max_pooling for gradient transfer
+    let outputSize = {
+      y : 1+(matrix.length - filter.length)/step.y,
+      x : 1+(matrix[0].length - filter[0].length)/step.x
+    }
+    //checking if convolution is possible
+    if(outputSize.y-Math.floor(outputSize.y) !== 0 || outputSize.x-Math.floor(outputSize.x) !== 0 ) throw "Err: size not compatible with " + type;
+    let output = new Array(outputSize.y).fill(0).map(e=>new Array(outputSize.x).fill(0))//convoluted output
+    //types of convolutions
+    for(let y = 0; y < outputSize.y; y++){
+      mask[y] = []
+      for(let x = 0; x < outputSize.x; x++){
+        if(type==="conv") output[y][x] = Activation[0](filterDot(x,y,matrix,filter) + bias);
+        else if(type==="full_conv")output[y][x] = Activation[0](filterDot(outputSize.x-(x+1),outputSize.y-(y+1),matrix,filter) + bias);
+        else if(type==="max_pool"){
+          mask[y][x] = {value:0,position:{x:0,y:0}};
+          for(let f1 = 0; f1 < 2; f1++){
+            for(let f2 = 0; f2 < 2; f2++){
+              if(matrix[f1+y*step.y][f2+x*step.x]>=mask[y][x].value){
+                mask[y][x].value = matrix[f1+y*step.y][f2+x*step.x];
+                mask[y][x].position.x = f2+x*step.x;
+                mask[y][x].position.y = f1+y*step.y;
+              }
+            }
+          }
+          output[y][x] = mask[y][x].value + bias
+        }
+        else throw "Err: unidentified type"
+      }
+    }
+    return (type==="max_pool")?[output,mask.flat()]:output
+  }
+  //func for CNN
+  convolutionLayers({matrix,kernal,featureMaps,stride,padding=0,type,activation} = {}){
+    let conv1 = []
+    for(let j = 0; j < matrix.length; j++){
+      conv1[j] = []
+      for(let i = 0; i < featureMaps; i++){
+        conv1[j][i] = this.Convolution({
+          matrix : matrix[j],
+          filter : kernal[i],
+          step : stride,
+          padding : padding,
+          type : type,
+          activation : activation
+        })
+      }
+    }
+    return conv1.flat()
+  }
+  forwordPass({image,layer_nodes,output_nodes,desired_outputs,learning_rate}){
+    if(image.length !== 1) throw "Err: Currently only type (1 x H x W) input supported"
+    this.ConvLayers = {Layer_0 : image}
+    this.learning_rate = learning_rate;
+    //parsing input given and applying filters (aka feed forward of CNN)
+    for(let i = 0; i < this.network.length; i++){
+      let layer = this.network[i];
+      this.ConvLayers[`Layer_${i+1}`] = this.convolutionLayers({
+        matrix : this.ConvLayers[`Layer_${i}`],
+        kernal : this.Filters[i],
+        featureMaps : this.featureMapCount[i],
+        stride : this.strides[i],
+        padding : 0,
+        type : layer[0],
+        activation : (layer[0] !== "max_pool")?layer[1]:"relu"
+      })
+    }
+    //reshaping inputs for neural network training
+    [this.FullyConnected,this.FCshape] = this.flattenFeatureMaps((this.network[this.network.length - 1][0] === "max_pool")?this.ConvLayers.Layer_4[0]:this.ConvLayers.Layer_4)
 
-// const Networks = {
-//   DNN:NeuralNetwork,
-//   CNN:ConvolutionNeuralNetwork
-// }
-module.exports = NeuralNetwork;
+    //creating neural network for fully connected parts
+    this.FCnetwork = new NeuralNetwork({
+      input_nodes : this.FullyConnected.length,
+      layer_count : layer_nodes,
+      output_nodes : output_nodes,
+      weight_bias_initilization_range : [-1,1]
+    });
+    //train once
+    let dnn = this.FCnetwork.trainIteration({
+      input : this.FullyConnected,
+      desired : desired_outputs,
+      learning_rate : this.learning_rate
+    })
+    return dnn
+  }
+  backwordPass(){
+    //reshaping inputs to matrix for convolutional training
+    this.LayerGrad = new Array(Object.keys(this.ConvLayers).length).fill(0).map(e=>0)
+    this.FilterGrad = new Array(this.Filters.length).fill(0).map(e=>0)
+
+    this.LayerGrad[this.LayerGrad.length-1] = this.FCnetwork.getInputGradients()
+    this.LayerGrad[this.LayerGrad.length-1] = this.reconstructMatrix(this.LayerGrad[this.LayerGrad.length-1],this.FCshape)
+    //backword pass for CNN
+    for(let i = this.Filters.length-1; i > -1 ; i--){
+      this.LayerGrad[i] = this.Convolution({
+        matrix : this.Rotate(this.Filters[i][0]), //rotates filter
+        filter : this.LayerGrad[i+1],
+        bias : 0,
+        step : {x:1,y:1},
+        padding : this.LayerGrad[i+1].length-1,
+        type : "full_conv",
+        activation : "linear"
+      });
+      this.Rotate(this.Filters[i][0])//rotates again so its "normal"
+      this.FilterGrad[i] = this.Convolution({
+        matrix : this.ConvLayers[`Layer_${i}`][0],
+        filter : this.LayerGrad[i+1],
+        bias : 0,
+        step : {x:1,y:1},
+        padding : 0,
+        type : "conv",
+        activation : "linear"
+      });
+    }
+    this.FilterGrad = this.FilterGrad.map(e=>[e])
+    this.Filters = this.addMatrix(this.Filters,this.FilterGrad,this.learning_rate)
+  }
+  //matrix manupliation
+  flattenFeatureMaps(featureMapMatrix){
+    let connected = [];
+    let shape = {y:featureMapMatrix[0].length,x:featureMapMatrix[0][0].length}
+    for(let featurePlane of featureMapMatrix) connected.push(featurePlane.flat())
+    return [connected.flat(),shape]
+  }
+  async processImage(path){
+    const pixels = require('image-pixels')
+    let {data, width, height} = await pixels(path)
+
+    let local_newImg_R = [], local_newImg_G = [], local_newImg_B = [],
+    newImg_R = [], newImg_G = [], newImg_B = [];
+    for(let i = 0; i < data.length/4; i++){
+      local_newImg_R[i] = data[4*i]/255;
+      local_newImg_G[i] = data[4*i+1]/255;
+      local_newImg_B[i] = data[4*i+2]/255;
+    }
+    //formatting into correct form
+    for(let i = 0; i < local_newImg_R.length/width; i++){
+      newImg_R[i] = [];
+      newImg_G[i] = [];
+      newImg_B[i] = [];
+      for(let j = 0; j < width; j++){
+        newImg_R[i][j] = local_newImg_R[j + width*i]
+        newImg_G[i][j] = local_newImg_G[j + width*i]
+        newImg_B[i][j] = local_newImg_B[j + width*i]
+      }
+    }
+    return [newImg_R,newImg_G,newImg_B]
+  }
+  createMatrix(x,y,z){
+    let M = []
+    for(let i = 0; i < z; i++){
+      M[i] = []
+      for(let j = 0; j < y; j++){
+        M[i][j] = []
+        for(let k = 0; k < x; k++){
+          M[i][j][k] = Math.random()*2 - 1
+        }
+      }
+    }
+    return M;
+  }
+  reconstructMatrix(flatArr,m,Matrix=[]){
+    for(let i = 0; i < m.y; i++){
+      Matrix[i] = []
+      for(let j = 0; j < m.x; j++){
+        Matrix[i][j] = flatArr[j + m.x*i]
+      }
+    }
+    return Matrix
+  }
+  Rotate(matrix){ // rotates matrix by 180
+    for(let i = 0; i < Math.floor(matrix.length/2); i++){
+      let a = matrix[i].slice()
+      let b = matrix[matrix.length - 1 - i].slice()
+      matrix[i] = b;
+      matrix[matrix.length - 1 - i] = a;
+    }
+    return matrix;
+  }
+  addMatrix(M1,M2,constant){ // M1 to be updated from M2
+    for(let z in M1){
+      for(let y in M1[z]){
+        for(let x in M1[z][y]){
+          for(let w in M1[z][y][x]){
+            M1[z][y][x][w] += -constant*M2[z][y][x][w];
+          }
+        }
+      }
+    }
+    return M1
+  }
+}
+
+try{
+  let x = new ConvolutionNeuralNetwork({
+    conv_layers :  [["conv","linear",3],["conv","linear",3],["conv","linear",3],["conv","linear",3]],
+    feature_maps : [          1      ,         1       ,           1     ,          1         ],
+    strides :      [    {x:1,y:1}     ,    {x:1,y:1}    ,      {x:1,y:1}    ,   {x:1,y:1} ]
+  })
+  let dnn = []
+  let image = x.createMatrix(10,10,1)
+
+  for(let k = 0; k < 200000; k++){
+    dnn[k] = x.forwordPass({
+      image : image,
+      layer_nodes : [],
+      output_nodes : 2,
+      desired_outputs : new Array(2).fill(0),
+      learning_rate : 0.03
+    })
+    x.backwordPass()
+  }
+  console.log(x.FCnetwork);
+  console.log(x);
+  dnn.map(e=>console.log(e.Cost))
+}catch(e){console.log(e)}

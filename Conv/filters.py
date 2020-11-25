4d6f4050de632a0e465cbf57a7b5ba94d1741b6c
Death-Raider <65229622+Death-Raider@users.noreply.github.com>
2020-11-25 22:07:39 +0530

    Update filters.py

--- Conv/filters.py
+++ Conv/filters.py
@@ -16,16 +16,3 @@ for filter in Filters:
             imgSave_value[x,y] = (int(filter[y][x]*255),int(filter[y][x]*255),int(filter[y][x]*255))
     imgSave.save(f"Filters/filter{i}.png")
     i+=1
-
-with open("Filters/Filters_BEFORE.txt") as File:
-    Filters = np.array(json.loads(File.read()))
-    i = 0
-    for filter in Filters:
-        filter = np.array(filter)
-        imgSave = Image.new("RGB",(len(filter),len(filter)),"black")
-        imgSave_value = imgSave.load()
-        for x in range(imgSave.width):
-            for y in range(imgSave.height):
-                imgSave_value[x,y] = (int(filter[y][x]*255),int(filter[y][x]*255),int(filter[y][x]*255))
-        imgSave.save(f"Filters/filter{i}_BEFORE.png")
-        i+=1
